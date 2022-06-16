017743a1a4cfe1e9608a385d41a79a2ae7c866bd
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2022-06-16 17:08:01 +0200

    Use wiremock to test http client

---
+++ src/test/java/io/github/warownia1/simplehttpclient/impl/HttpClientSendTest.java
@@ -0,0 +1,200 @@
+package io.github.warownia1.simplehttpclient.impl;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.http.RequestMethod;
+import com.github.tomakehurst.wiremock.matching.RequestPatternBuilder;
+import io.github.warownia1.simplehttpclient.HttpClient;
+import io.github.warownia1.simplehttpclient.HttpHeaders;
+import io.github.warownia1.simplehttpclient.HttpResponse;
+import org.testng.annotations.*;
+
+import java.io.IOException;
+import java.net.ProtocolException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+
+public class HttpClientSendTest {
+  WireMockServer server;
+
+  @BeforeClass
+  public void setupServer() {
+    server = new WireMockServer(wireMockConfig().dynamicPort());
+    server.start();
+    WireMock.configureFor(server.port());
+  }
+
+  @AfterClass
+  public void teardownServer() {
+    server.stop();
+  }
+
+  @BeforeMethod
+  public void resetWireMock() {
+    WireMock.reset();
+  }
+
+  @DataProvider(name = "SimplePath")
+  public Object[][] createSimplePath() {
+    return new Object[][]{
+        {"/"},
+        {"/index"},
+        {"/index.html"},
+        {"/path/to/resource"},
+        {"/page/15"},
+        {"/alpha%20bravo"},
+        {"/alpha-bravo"},
+        {"/?key=val"},
+        {"/?param1=A&param2=B"},
+        {"/index?key=val"},
+    };
+  }
+
+  @Test(dataProvider = "SimplePath")
+  public void send_SimplePath_RequestPathMatches(String path)
+      throws IOException {
+    stubFor(get(path).willReturn(ok()));
+    var client = HttpClient.newHttpClient();
+    var request = new HttpRequestStub(server.url(path));
+    client.send(request, HttpResponse.BodyHandlers.discarding());
+    verify(getRequestedFor(urlEqualTo(path)));
+  }
+
+  @Test
+  public void send_EmptyPath_PathIsRoot() throws IOException {
+    stubFor(get("/").willReturn(ok()));
+    var client = HttpClient.newHttpClient();
+    var request = new HttpRequestStub(server.url(""));
+    client.send(request, HttpResponse.BodyHandlers.discarding());
+    verify(getRequestedFor(urlEqualTo("/")));
+  }
+
+  @Test(dataProvider = "SimplePath")
+  public void send_PathWithFragment_FragmentNotSent(String path)
+      throws IOException {
+    stubFor(get(path).willReturn(ok()));
+    var client = HttpClient.newHttpClient();
+    var request = new HttpRequestStub(server.url(path) + "#fragment");
+    client.send(request, HttpResponse.BodyHandlers.discarding());
+    verify(getRequestedFor(urlEqualTo(path)));
+  }
+
+  @DataProvider(name = "HttpMethod")
+  public Object[][] createHttpMethod() {
+    return new Object[][]{
+        {"GET"},
+        {"HEAD"},
+        {"POST"},
+        {"PUT"},
+        {"DELETE"},
+        {"OPTIONS"},
+        {"TRACE"},
+        {"PATCH"}, // unfortunately HttpURLConnection does not allow PATCH
+    };
+  }
+
+  @Test(dataProvider = "HttpMethod")
+  public void send_TestMethodSent(String method) throws IOException {
+    stubFor(any(urlEqualTo("/")).willReturn(ok()));
+    var client = HttpClient.newHttpClient();
+    var request = new HttpRequestStub(server.baseUrl());
+    request.method = method;
+    client.send(request, HttpResponse.BodyHandlers.discarding());
+    verify(RequestPatternBuilder.newRequestPattern(
+        RequestMethod.fromString(method), urlEqualTo("/")));
+  }
+
+  @DataProvider(name = "InvalidHttpMethod")
+  public Object[][] createInvalidHttpMethod() {
+    return new Object[][]{
+        {"CUSTOM"},
+        {"get"},
+        {"A METHOD"}
+    };
+  }
+
+  @Test(dataProvider = "InvalidHttpMethod", expectedExceptions = ProtocolException.class)
+  public void send_InvalidHttpMethod_ThrowProtocolException(String method)
+      throws IOException {
+    var client = HttpClient.newHttpClient();
+    var request = new HttpRequestStub(server.baseUrl());
+    request.method = method;
+    client.send(request, HttpResponse.BodyHandlers.discarding());
+  }
+
+  @DataProvider(name = "RequestContent")
+  public Object[][] createRequestContent() {
+    var strings = new String[]{
+        "",
+        "Content",
+        "Request Content",
+        "param1=val&param2=val",
+        "Multi\r\nline\r\ncontent",
+        "úñìÇóÐË",
+    };
+    var args = new Object[strings.length][];
+    for (int i = 0; i < strings.length; i++) {
+      args[i] = new Object[]{strings[i].getBytes(StandardCharsets.UTF_8)};
+    }
+    return args;
+  }
+
+  @Test(dataProvider = "RequestContent")
+  public void send_SendBody_ContentReceived(byte[] body) throws IOException {
+    stubFor(post("/").willReturn(ok()));
+    var client = HttpClient.newHttpClient();
+    var request = new HttpRequestStub(server.baseUrl());
+    request.method = "POST";
+    request.body = new ByteArrayRequestBody(body);
+    client.send(request, HttpResponse.BodyHandlers.discarding());
+    verify(postRequestedFor(urlPathEqualTo("/"))
+        .withRequestBody(binaryEqualTo(body)));
+  }
+
+  @Test(dataProvider = "RequestContent")
+  public void send_SendBody_ContentLengthSet(byte[] body) throws IOException {
+    stubFor(post("/").willReturn(ok()));
+    var client = HttpClient.newHttpClient();
+    var request = new HttpRequestStub(server.baseUrl());
+    request.method = "POST";
+    request.body = new ByteArrayRequestBody(body);
+    client.send(request, HttpResponse.BodyHandlers.discarding());
+    verify(postRequestedFor(urlPathEqualTo("/"))
+        .withHeader("Content-Length", equalTo(Integer.toString(body.length))));
+  }
+
+  @DataProvider(name = "Headers")
+  public Object[][] createHeaderValues() {
+    return new Object[][]{
+        {Map.of("A", List.of("B"))},
+        {Map.of("A", List.of("B", "C"))},
+        {Map.of("A", List.of("B", "C", "D"))},
+        {Map.of("A", List.of("B"), "X", List.of("Y"))},
+        {Map.of("A", List.of("B"), "X", List.of("Y", "Z"))},
+        {Map.of("A", List.of("B", "C"), "X", List.of("Y", "Z", "T"))}
+    };
+  }
+
+  @Test(dataProvider = "Headers")
+  public void send_SetHeaders_HeadersPresent(Map<String, List<String>> headersMap)
+      throws IOException {
+    stubFor(get("/").willReturn(ok()));
+    var client = HttpClient.newHttpClient();
+    var request = new HttpRequestStub(server.baseUrl());
+    request.headers = HttpHeaders.of(headersMap);
+    client.send(request, HttpResponse.BodyHandlers.discarding());
+    var reqPattern = getRequestedFor(urlEqualTo("/"));
+    for (var entry : headersMap.entrySet()) {
+      String headerName = entry.getKey();
+      for (var headerValue : entry.getValue()) {
+        reqPattern = reqPattern.withHeader(headerName, equalTo(headerValue));
+      }
+    }
+    verify(reqPattern);
+  }
+}
